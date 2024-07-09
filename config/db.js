e7e9f476a532e0bac97992484ae1d760472bbcc8
Amadha Prabuddhini <153179645+Amaa0915@users.noreply.github.com>
2024-07-09 11:10:40 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+
+const dburl = "mongodb+srv://amadha:1234@cluster0.lnio0ko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
+
+const connection = async () => {
+
+
+    
+    try {
+        await mongoose.connect(dburl);
+        console.log("MongoDB Connect~");
+    } catch(e) {
+        console.error(e.message);
+        process.exit();
+    }
+
+
+};
+
+module.exports = connection;
